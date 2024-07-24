55c2116bd6e0181b0359859be705bf894322e2eb
Venkidesh Venu <venkidesh.venu.04@gmail.com>
2024-07-24 22:13:15 +0530

    Homepage contents added and partially completed the app

--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def get_home_page(request):
+    return render(request, 'temp_homepage/homepage.html',context={})
