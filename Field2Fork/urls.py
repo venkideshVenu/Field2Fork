b1a63c27cfe4a7894721180c470a2d1ded78b814
Venkidesh Venu <venkidesh.venu.04@gmail.com>
2024-07-24 22:46:30 +0530

    Minor changes on app homepage

--- Field2Fork/urls.py
+++ Field2Fork/urls.py
@@ -22,5 +22,5 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include('homepage.urls')),
+    path('', include('homepage.urls')),
 ] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
