adf4d5d4b6452d2ff1713a165467c9e60a0a07be
joelgeema-3130 <joelgeema2004@gmail.com>
2024-07-25 23:10:08 +0530

    joel

--- Field2Fork/settings.py
+++ Field2Fork/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'homepage',
     'marketplace',
+    'categories',
     'accountsAndLogin',
 ]
 
--- marketplace/views.py
+++ marketplace/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
-
+from marketplace.models import Product
+from marketplace.models import Categories
 # Create your views here.
+
+def market_place(request):
+    return render(request, 'store/market_place.html' ,context={})
+
+def store(request, category_slug=None,cart_cost=0,cart_items=0):
+    category = None
+    products = None
+    cart_cost = 2000
+    cart_items = 5
+
+    if category_slug != None:
+        category = get_object_or_404(Categories, slug=category_slug )
+        products = Product.objects.filter(category=category , is_available= True) 
+        product_count = products.count()
+    else:
+        products = Product.objects.all().filter(is_available= True)
+        product_count = products.count()
+
+    context = {
+        'products' : products,
+        'product_count' : product_count,
+        'category' : category,
+        'selcted_slug' : category_slug,
+        'cart_cost': cart_cost,
+        'cart_items': cart_items,
+        'total_items': len(CartItem.objects.all()),
+    }
+
+    return render(request, "store/store.html", context)
+
+
+def product_detail(request, category_slug, product_slug):
+    try:
+        single_product = Product.objects.get(category__slug = category_slug, slug = product_slug)
+    except Exception as e:
+        raise e
+    
+    context = {
+        'single_product' : single_product,
+        'total_items': len(CartItem.objects.all()),
+
+    }
+
+    return render(request, "store/product_detail.html",context)
