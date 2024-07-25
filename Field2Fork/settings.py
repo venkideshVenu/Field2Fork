576eb942b9630dab05740350af46903920be80c0
Venkidesh Venu <venkidesh.venu.04@gmail.com>
2024-07-25 21:48:45 +0530

    Partially Completed accountsAndLogin App

--- Field2Fork/settings.py
+++ Field2Fork/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'homepage',
     'marketplace',
-    'loginAndAccounts',
+    'accountsAndLogin',
 ]
 
 MIDDLEWARE = [
--- Field2Fork/urls.py
+++ Field2Fork/urls.py
@@ -23,4 +23,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('homepage.urls')),
+    path('account/',include('accountsAndLogin.urls')),
 ] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
