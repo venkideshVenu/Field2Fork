6e17deddfb880f7ba81d2617541938be49bdc596
Venkidesh Venu <venkidesh.venu.04@gmail.com>
2024-07-27 14:36:02 +0530

    Updated Langing Page and Added About and Contact page

--- homepage/urls.py
+++ homepage/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.get_home_page, name="homepage"),
+    path('about/', views.get_about_page, name="aboutpage"),
+    path('contact/', views.get_contact_page, name="contactpage"),
+    path('pagenotfound/', views.get_error_page, name="notfound"),
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -3,4 +3,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def get_home_page(request):
-    return render(request, 'temp_homepage/homepage.html',context={})
+    return render(request, 'temp_homepage/home.html',context={})
+
+def get_about_page(request):
+    return render(request, 'temp_homepage/about.html',context={})
+
+def get_contact_page(request):
+    return render(request, 'temp_homepage/contact.html',context={})
+
+def get_error_page(request):
+    return render(request, 'temp_homepage/404.html',context={})
